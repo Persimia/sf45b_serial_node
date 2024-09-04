87e574408a0ff42f52f64e4369cb71fc1b0e586d
Sam Kemp <samkemp1996@gmail.com>
2024-09-04 11:15:13 -0400

    testing

--- src/spoofer.cpp
+++ src/spoofer.cpp
@@ -1,10 +1,33 @@
-#include <cstdio>
+#include <memory>
 
-int main(int argc, char ** argv)
+#include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/string.hpp"
+#include "sensor_msgs/msg/laser_scan.hpp"
+
+using std::placeholders::_1;
+
+class MinimalSubscriber : public rclcpp::Node
 {
-  (void) argc;
-  (void) argv;
+  public:
+    MinimalSubscriber()
+    : Node("minimal_subscriber")
+    {
+      subscription_ = this->create_subscription<sensor_msgs::msg::LaserScan>(
+      "scan", 10, std::bind(&MinimalSubscriber::topic_callback, this, _1));
+    }
 
-  printf("hello world sf45b_serial_node package\n");
+  private:
+    void topic_callback(const sensor_msgs::msg::LaserScan::SharedPtr msg) const
+    {
+      RCLCPP_INFO(this->get_logger(), "I heard: '%f'", msg->ranges[0]);
+    }
+    rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr subscription_;
+};
+
+int main(int argc, char * argv[])
+{
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<MinimalSubscriber>());
+  rclcpp::shutdown();
   return 0;
 }
